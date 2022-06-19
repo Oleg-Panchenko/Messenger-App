6bd9ccbefa028614c47254be3af41b389adef2ec
Panchenko Oleg <panoleg91@mail.ru>
2022-06-19 18:12:37 +0300

    Initial Commit

---
+++ IChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by Panchenko Oleg on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
